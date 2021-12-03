3ee412d979937282144fa031d395e02350db3e4d
Taras <slovak.taras@gmail.com>
2021-12-03 18:50:07 +0200

    refactoring

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,15 +1,15 @@
+import {$} from '@core/Dom';
+
 export class Excel {
   constructor(selector, options) {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
   }
   getRoot() {
-    const $root = document.createElement('div');
-    $root.classList.add('excel');
+    const $root = $.create( 'div', 'excel');
 
     this.components.forEach(Component=>{
-      const $el = document.createElement('div');
-      $el.classList.add(Component.className);
+      const $el = $.create('div', Component.className);
       const component = new Component($el);
       $el.innerHTML = component.toHTML();
       $root.append($el);
