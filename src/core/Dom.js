05f9626f5f6ce61202dfd48b32bd45d8ff28a206
Taras <slovak.taras@gmail.com>
2021-12-04 18:29:31 +0200

    off

--- src/core/Dom.js
+++ src/core/Dom.js
@@ -1,9 +1,9 @@
 class Dom {
   constructor(selector) {
     // #app
-    this.$el = typeof selector ==='string' ?
-    document.querySelector(selector):
-    selector;
+    this.$el = typeof selector ==='string'
+      ? document.querySelector(selector)
+      : selector;
   }
 }
 
