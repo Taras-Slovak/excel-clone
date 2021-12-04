a04680b14e2c55c64081ea0e59ab57a77692c196
Taras <slovak.taras@gmail.com>
2021-12-04 20:11:14 +0200

    add constructor

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,6 +1,9 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options) {
+    super($root);
+  }
   toHTML() {
     return '';
   }
