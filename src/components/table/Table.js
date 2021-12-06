8807fb5f0646cb9d7a2d64d9c620ea641c4e7ee7
Taras <slovak.taras@gmail.com>
2021-12-06 19:25:25 +0200

    start to create  resize for table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,20 @@ import {createTable} from '@/components/table/table.template';
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown']
+    });
+  }
+
   toHTML() {
     return createTable(20);
+  }
+  onClick() {
+    cosole.log('click');
+  }
+
+  onMouseDown() {
+    cosole.log('mousedown');
   }
 }
