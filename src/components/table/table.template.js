f40afc7a68e16a372e5e51de0a4a6206584d107c
Taras <slovak.taras@gmail.com>
2021-12-08 13:48:12 +0200

    add new data-attributes

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,13 +3,13 @@ const CODES = {
   Z: 90
 };
 
-function toCell() {
-  return `<div class="cell" contenteditable></div>`;
+function toCell(_, col) {
+  return `<div class="cell" contenteditable data-col="${col}"></div>`;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column" data-type="resizable">
+    <div class="column" data-type="resizable" data-col="${index}">
       ${col}
       <div class="col-resize"data-resize="col"></div>
     </div>`;
