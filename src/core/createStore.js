8d82c666e5469835f13a8d95542319c9136833ea
Taras <slovak.taras@gmail.com>
2021-12-14 15:29:15 +0200

    add subscribe() for function createStore()

--- src/core/createStore.js
+++ src/core/createStore.js
@@ -1,5 +1,16 @@
 export function createStore() {
+  const state = {};
+  let listeners = [];
   return {
-  
-  }
+    subscribe(fn) {
+      listeners.push(fn);
+      return {
+        unsubscribe() {
+          listeners = listeners.filter(l => l !==fn );
+        }
+      };
+    },
+    dispatch() {},
+    getState() {}
+  };
 }
