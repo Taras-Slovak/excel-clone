1131f954403da01306009e0a3a829e4e37c0a37b
Taras <slovak.taras@gmail.com>
2021-12-14 16:54:41 +0200

    add code for index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
+import {createStore} from '@core/createStore';
+
+const store = createStore();
 
 const excel =	new Excel('#app', {
   components: [Header, Toolbar, Formula, Table]
