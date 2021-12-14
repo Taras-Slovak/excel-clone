a32817205815a7d08a1df35c4f5cffbab2909149
Taras <slovak.taras@gmail.com>
2021-12-14 17:06:56 +0200

    add ()

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,6 +5,7 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
+    this.store = options.store;
     this.unsubscribers = [];
 
     this.prepare();
@@ -33,6 +34,10 @@ export class ExcelComponent extends DomListener {
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn);
     this.unsubscribers.push(unsub);
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action);
   }
 
   // Deleting a component
