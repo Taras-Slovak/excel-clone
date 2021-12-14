572cd5ad5c7615ef8e6f9c6ce8a003103a6631f4
Taras <slovak.taras@gmail.com>
2021-12-14 17:19:01 +0200

    testing

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -36,11 +36,16 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus();
     });
+
+    this.$subscribe(state => {
+      console.log('TableState', state);
+    });
   }
 
   selectCell($cell) {
     this.selection.select($cell);
     this.$emit('table:select', $cell);
+    this.$dispatch({type: 'Test'});
   }
 
   onMousedown(event) {
