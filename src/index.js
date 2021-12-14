f3a527bac347013c80c7fcc2cded2d41b2e180d7
Taras <slovak.taras@gmail.com>
2021-12-14 16:57:20 +0200

    add code for index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import {createStore} from '@core/createStore';
+import {rootReducer} from '@/redux/rootReducer';
 
-const store = createStore();
+const store = createStore(rootReducer());
 
 const excel =	new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 });
 
 excel.render();
