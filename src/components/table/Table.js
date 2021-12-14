49d15d5f154e67906c7ca1b0fd4f8ce28626651e
Taras <slovak.taras@gmail.com>
2021-12-14 20:39:00 +0200

    change state

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -5,6 +5,7 @@ import {isCell, matrix,
   nextSelector, shouldResize} from '@/components/table/table.function';
 import {TableSelection} from '@/components/table/TableSelection';
 import {$} from '@core/Dom';
+import * as actions from '@/redux/action';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -50,7 +51,7 @@ export class Table extends ExcelComponent {
   async resizeTable(event) {
     try {
       const data = await resizeHandler(this.$root, event);
-      this.$dispatch({type: 'TABLE_RESIZE', data});
+      this.$dispatch(actions.tableResize(data));
     } catch (e) {
       console.warn('Resize error', e.message);
     }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import {createStore} from '@core/createStore';
 import {rootReducer} from '@/redux/rootReducer';
 import {storage} from '@core/utils';
 
-const store = createStore(rootReducer, {
-  colState: {}
-});
+const store = createStore(rootReducer, storage('excel-state')
+);
 
 store.subscribe(state => {
   console.log('App State:', state);
