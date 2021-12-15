ea8a12d6af60470f9b7acacfda98103e38db2fc1
Taras <slovak.taras@gmail.com>
2021-12-15 19:27:01 +0200

    refactoring

--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -41,7 +41,7 @@ export function resizeHandler($root, event) {
       resolve({
         value,
         type,
-        id: type === 'col' ? $parent.data.col: $parent.data.row
+        id: $parent.data[type]
       });
 
       $resizer.css({
