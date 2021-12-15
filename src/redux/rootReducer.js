eecd0239d5e4aa939ea5c3c4d65327ce3a1654c1
Taras <slovak.taras@gmail.com>
2021-12-15 19:22:05 +0200

    fix

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -7,7 +7,7 @@ export function rootReducer(state, action) {
   switch (action.type) {
     case TABLE_RESIZE:
       field = action.data.type === 'col' ? 'colState' : 'rowState';
-      prevState = state.[field] || {};
+      prevState = state[field] || {};
       prevState[action.data.id] = action.data.value;
       return {...state, [field]: prevState};
     default: return state;
