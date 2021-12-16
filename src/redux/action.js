cede27061d882f9636dc824c847f18cf8d8332eb
Taras <slovak.taras@gmail.com>
2021-12-16 21:16:07 +0200

    function changeText(text)

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,9 +1,16 @@
-import {TABLE_RESIZE} from '@/redux/type';
+import {CHANGE_TEXT, TABLE_RESIZE} from '@/redux/type';
 
 // Action creator
 export function tableResize(data) {
   return {
     type: TABLE_RESIZE,
     data
+  };
+}
+
+export function changeText(text) {
+  return {
+    type: CHANGE_TEXT,
+    data: text
   };
 }
