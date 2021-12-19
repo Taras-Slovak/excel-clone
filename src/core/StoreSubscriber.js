1d934482f2403ccab10faad87134c0c32a75ab64
Taras <slovak.taras@gmail.com>
2021-12-19 11:41:34 +0200

    testing

--- src/core/StoreSubscriber.js
+++ src/core/StoreSubscriber.js
@@ -2,10 +2,14 @@ export class StoreSubscriber {
   constructor(store) {
     this.store = store;
     this.sub = null;
+    this.prevState = {};
   }
   subscribeComponents(components) {
+    this.prevState = this.store.getStore();
     this.sub = this.store.subscribe(state => {
-
+      Object.keys(state).forEach(key => {
+        console.log(key);
+      });
     });
   }
   unsubscribeFromStore() {
