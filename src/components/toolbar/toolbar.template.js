1286354983548e788b438d2e93339c8ad09e6050
Taras <slovak.taras@gmail.com>
2021-12-20 13:18:25 +0200

    add code for toolbar.template.js

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -1,4 +1,31 @@
+function toButton(button) {
+  return `
+    <div class="button">
+      <i class="material-icons">${button.icon}</i>
+    </div>`;
+}
+
 export function createToolbar() {
+  const buttons = [
+    {
+      icon: 'format_align_left'
+    },
+    {
+      icon: 'format_align_center'
+    },
+    {
+      icon: 'format_align_right'
+    },
+    {
+      icon: 'format_bold'
+    },
+    {
+      icon: 'format_italic'
+    },
+    {
+      icon: 'format_underlined'
+    },
+  ];
   return `
       <div class="button">
           <i class="material-icons">format_align_left</i>
