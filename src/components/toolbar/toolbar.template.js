cae409004ee82c75f051dc622d03fcb6068c2863
Taras <slovak.taras@gmail.com>
2021-12-20 13:14:42 +0200

    refactoring

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -1,3 +1,27 @@
 export function createToolbar() {
-
+  return `
+      <div class="button">
+          <i class="material-icons">format_align_left</i>
+      </div>
+      
+      <div class="button">
+          <i class="material-icons">format_align_center</i>
+      </div>
+      
+      <div class="button">
+          <i class="material-icons">format_align_right</i>
+      </div>
+      
+      <div class="button">
+          <i class="material-icons">format_bold</i>
+      </div>
+      
+      <div class="button">
+          <i class="material-icons">format_italic</i>
+      </div>
+      
+      <div class="button">
+          <i class="material-icons">format_underlined</i>
+      </div>
+  `;
 }
