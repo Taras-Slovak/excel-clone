79fd365a7b489c78aa0142a63502fd0255a633d3
Taras <slovak.taras@gmail.com>
2021-12-22 15:11:35 +0200

    change the name of the table

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,5 @@
 import {
-  CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE
+  CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE
 } from './types';
 
 // Action Creator
@@ -28,6 +28,13 @@ export function changeStyles(data) {
 export function applyStyle(data) {
   return {
     type: APPLY_STYLE,
+    data
+  };
+}
+
+export function changeTitle(data) {
+  return {
+    type: CHANGE_TITLE,
     data
   };
 }
